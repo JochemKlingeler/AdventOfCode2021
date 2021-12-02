a6ddee45eef0c2af652ad39c5feb3d1c02f30758
Jochem Klingeler <jochem.klingeler@gmail.com>
2021-12-02 20:41:04 +0100

    Day 2 part 2 DONE

--- src/day_02/mod.rs
+++ src/day_02/mod.rs
@@ -30,6 +30,40 @@ fn do_part1(input: &str) -> i32 {
     x * y
 }
 
+pub fn part2() -> i32 {
+    do_part2(&get_input())
+}
+
+fn do_part2(input: &str) -> i32 {
+    let mut x = 0;
+    let mut y = 0;
+    let mut aim = 0;
+    let lines = input.lines();
+    lines.for_each(|s| {
+        let mut split = s.splitn(2, ' ');
+        let (direction, unit) = (
+            split
+                .next()
+                .expect("Expected to be able to parse direction"),
+            split
+                .next()
+                .expect("Expected to be able to parse unit")
+                .parse::<i32>()
+                .expect("Expected to be able to cast unit"),
+        );
+        match direction {
+            "forward" => {
+                x += unit;
+                y += unit * aim
+            }
+            "down" => aim += unit,
+            "up" => aim -= unit,
+            _ => panic!("Invalid direction: {}", direction),
+        }
+    });
+    x * y
+}
+
 fn get_input() -> String {
     fs::read_to_string("src/day_02/input.txt").expect("Could not load input file!")
 }
@@ -43,6 +77,21 @@ mod tests {
         assert_eq!(
             150,
             do_part1(
+                "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2"
+            )
+        )
+    }
+
+    #[test]
+    fn part2_demo() {
+        assert_eq!(
+            900,
+            do_part2(
                 "forward 5
 down 5
 forward 8
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,5 @@
 mod day_02;
 fn main() {
     println!("Day 2, part 1: {}", day_02::part1());
+    println!("Day 2, part 2: {}", day_02::part2());
 }
