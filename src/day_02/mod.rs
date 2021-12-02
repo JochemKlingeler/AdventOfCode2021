74854bf12d8b08f5e305fdb1b69ffc959a514c16
Jochem Klingeler <jochem.klingeler@gmail.com>
2021-12-02 20:18:40 +0100

    Day 2 part 1 DONE

--- src/day_02/mod.rs
+++ src/day_02/mod.rs
@@ -1,9 +1,55 @@
-#![warn(clippy::pedantic)]
-pub fn part1() -> usize
-{
-    do_part1(" ")
+use std::fs;
+
+pub fn part1() -> i32 {
+    do_part1(&get_input())
 }
-fn do_part1(input: &str) -> usize
-{
-    4
+
+fn do_part1(input: &str) -> i32 {
+    let mut x = 0;
+    let mut y = 0;
+    let lines = input.lines();
+    lines.for_each(|s| {
+        let mut split = s.splitn(2, ' ');
+        let (direction, unit) = (
+            split
+                .next()
+                .expect("Expected to be able to parse direction"),
+            split
+                .next()
+                .expect("Expected to be able to parse unit")
+                .parse::<i32>()
+                .expect("Expected to be able to cast unit"),
+        );
+        match direction {
+            "forward" => x += unit,
+            "down" => y += unit,
+            "up" => y -= unit,
+            _ => panic!("Invalid direction: {}", direction),
+        }
+    });
+    x * y
+}
+
+fn get_input() -> String {
+    fs::read_to_string("src/day_02/input.txt").expect("Could not load input file!")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_demo() {
+        assert_eq!(
+            150,
+            do_part1(
+                "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2"
+            )
+        )
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![warn(clippy::pedantic)]
+
 mod day_02;
 fn main() {
     println!("Day 2, part 1: {}", day_02::part1());
